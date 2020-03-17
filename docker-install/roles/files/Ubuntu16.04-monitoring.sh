1c0007e8073e006604fa7c1d9564ebdecf26c900
Mihai IDU <idumihai16@gmail.com>
2020-03-17 14:04:26 +0100

    minnor updates before testing

---
+++ docker-install/roles/files/Ubuntu16.04-monitoring.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# influxdb
+curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/lsb-release
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+
+#grafana
+echo "deb https://packagecloud.io/grafana/stable/debian/ wheezy main" | sudo tee /etc/apt/sources.list.d/grafana.list
+curl https://packagecloud.io/gpg.key | sudo apt-key add -
+
+#telegraf
+cat <<EOF | sudo tee /etc/apt/sources.list.d/influxdata.list
+deb https://repos.influxdata.com/ubuntu bionic stable
+EOF
+sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+sudo apt-get update -y
